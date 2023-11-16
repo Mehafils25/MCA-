2d0c97bfbe3977ed5db52963310cab0973b5708f
Mehafil Salim <150989601+Mehafils25@users.noreply.github.com>
2023-11-16 05:18:43 +0000

    Add files via upload

---
+++ Sem1/Python/p5.py
@@ -0,0 +1,8 @@
+input_string = input("enter a string:")
+char_frequency = {}
+for char in input_string:
+	char_frequency[char] = char_frequency.get(char,0) + 1
+print("charachter frequencies in the string :")
+for char, frequency in char_frequency.items():
+	print(char,frequency) 
+
