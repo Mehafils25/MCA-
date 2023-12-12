ae7b6b890a44fe6197e440305b9ae2efc8fb69f9
Mehafil Salim <150989601+Mehafils25@users.noreply.github.com>
2023-12-12 10:04:17 +0000

    Delete Sem1/Python directory

--- Sem1/Python/occalist.py
+++
@@ -1,6 +0,0 @@
-names=raw_input("enter a list of first names:")
-count = 0
-for name in names:
-	if 'z' in name:
-		count+=1
-print(count)
--- Sem1/Python/p5.py
+++
@@ -1,8 +0,0 @@
-input_string = input("enter a string:")
-char_frequency = {}
-for char in input_string:
-	char_frequency[char] = char_frequency.get(char,0) + 1
-print("charachter frequencies in the string :")
-for char, frequency in char_frequency.items():
-	print(char,frequency) 
-
